9135c419030c9de3ddfb713d59c23d8d2b7ca8c1
Adam Steel <adamdanielsteel@gmail.com>
2018-05-04 07:44:47 -0400

    clearing changes from local repo to update

--- visualization/corr_metab_gm.m
+++ visualization/corr_metab_gm.m
@@ -13,12 +13,12 @@ for i = 1:size(subjStruct.metab_map,4)
     [r,p] = corr(X,Y);
     %%
     figure;
-    scatter(X,Y,40,Y,'linewidth',3)
+    scatter(X,Y,40,'ko','filled','linewidth',3)
     hold on
     addLinFit(X,Y,[0.5 0.5 0.5],[0.2 max(Y)])
     title([subjStruct.subj ' ' metabName]);
     %ylim([0 15])
-    colormap('parula')
+    %colormap('parula')
     axis square
     formatFigAxis(gca,5,[],5,0,[min(X) max(Y)])
     %print(gcf,[subjStruct.subj '.' metabName '.scat.eps'],'-depsc')
@@ -31,12 +31,12 @@ for i = 1:size(subjStruct.metab_map,4)
     Y_below = Y(X < 0.1);
      [~,gm_wm_pv,~,gm_wm_t] = ttest2(Y_above,Y_below);
     % %%
-    % clr = lines(4);
-    % scatJitt({Y_below,Y_above},clr(1:2,:),[],0)
-    % title([subjStruct.subj ' ' metabName]);
-    % formatFigAxis(gca,5,[1 2],{'WM','GM'},0,[])
-    % text(0.55,min(Y_below),['t = ' sprintf('%.2f',gm_wm_t.tstat) ...
-    %     '; p = ' sprintf('%.2f',gm_wm_pv)],'fontsize',12)
+     clr = lines(4);
+     scatJitt({Y_below,Y_above},clr(1:2,:),[],0)
+     title([subjStruct.subj ' ' metabName]);
+     formatFigAxis(gca,5,[1 2],{'WM','GM'},0,[])
+     text(0.55,min(Y_below),['t = ' sprintf('%.2f',gm_wm_t.tstat) ...
+         '; p = ' sprintf('%.2f',gm_wm_pv)],'fontsize',12)
     % print(gcf,[subjStruct.subj '.' metabName '.split.eps'],'-depsc')
     % close
     %%
